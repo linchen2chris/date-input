e160576a81110f16b3581e79beae22b964191d0d
Chris Lin <lc1990linux@gmail.com>
2018-03-09 15:41:44 +0800

    [chen] add the simple 3 input fields

---
+++ src/DateInput.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class DateInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = this.parseDate(props);
+  }
+
+  parseDate(props) {
+      if (props.value !== undefined && props.value.match(/\d{0,4}-\d{0,2}-\d{0,2}/)) {
+          const dateArray = props.value.split('-');
+          return {
+              year: dateArray[0],
+              month: dateArray[1],
+              day: dateArray[2],
+              value: props.value,
+          };
+      }
+      return {
+          year: '',
+          month: '',
+          day: '',
+          value: '',
+      }
+  }
+
+  render() {
+    return (
+        <div>
+          <div id="day-container" >
+            <input
+              id={`${this.props.id}-day`}
+              value={this.state.day}
+              />
+              <span>/</span>
+          </div>
+            <div id="month-container" >
+                <input
+                    id={`${this.props.id}-month`}
+                    value={this.state.month}
+                />
+                <span>/</span>
+            </div>
+            <div id="year-container" >
+                <input
+                    id={`${this.props.id}-year`}
+                    value={this.state.year}
+                />
+                <span>/</span>
+            </div>
+        </div>
+    )
+  }
+}
+
+DateInput.propTypes = {
+  id: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  onBlur: PropTypes.func,
+};
