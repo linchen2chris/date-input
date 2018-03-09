2e674defedb308fa2179930074635330fc2c8926
Chris Lin <lc1990linux@gmail.com>
2018-03-09 16:08:50 +0800

    [chen] introduce storybook

--- src/DateInput.jsx
+++ src/DateInput.jsx
@@ -1,55 +1,55 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 class DateInput extends Component {
   constructor(props) {
     super(props);
-    this.state = this.parseDate(props);
+    this.state = this.parseDate(props.value);
   }
 
-  parseDate(props) {
-      if (props.value !== undefined && props.value.match(/\d{0,4}-\d{0,2}-\d{0,2}/)) {
-          const dateArray = props.value.split('-');
-          return {
-              year: dateArray[0],
-              month: dateArray[1],
-              day: dateArray[2],
-              value: props.value,
-          };
-      }
+  parseDate(value) {
+    if (value !== undefined && value.match(/\d{0,4}-\d{0,2}-\d{0,2}/)) {
+      const dateArray = value.split('-');
       return {
-          year: '',
-          month: '',
-          day: '',
-          value: '',
-      }
+        year: dateArray[0],
+        month: dateArray[1],
+        day: dateArray[2],
+        value: value,
+      };
+    }
+    return {
+      year: '',
+      month: '',
+      day: '',
+      value: '',
+    }
   }
 
   render() {
     return (
-        <div>
-          <div id="day-container" >
-            <input
-              id={`${this.props.id}-day`}
-              value={this.state.day}
-              />
-              <span>/</span>
-          </div>
-            <div id="month-container" >
-                <input
-                    id={`${this.props.id}-month`}
-                    value={this.state.month}
-                />
-                <span>/</span>
-            </div>
-            <div id="year-container" >
-                <input
-                    id={`${this.props.id}-year`}
-                    value={this.state.year}
-                />
-                <span>/</span>
-            </div>
+      <div>
+        <div id="day-container">
+          <input
+            id={`${this.props.id}-day`}
+            value={this.state.day}
+          />
+          <span>/</span>
         </div>
+        <div id="month-container">
+          <input
+            id={`${this.props.id}-month`}
+            value={this.state.month}
+          />
+          <span>/</span>
+        </div>
+        <div id="year-container">
+          <input
+            id={`${this.props.id}-year`}
+            value={this.state.year}
+          />
+          <span>/</span>
+        </div>
+      </div>
     )
   }
 }
@@ -60,3 +60,5 @@ DateInput.propTypes = {
   onChange: PropTypes.func,
   onBlur: PropTypes.func,
 };
+
+export default DateInput;
